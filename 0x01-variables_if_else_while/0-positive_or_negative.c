0faebac07c5b64b1f33e5e0c6c19da7fb6612e66
Alian416-code <2051@holbertonschool.com>
2020-06-14 05:29:47 +0000

    Veriables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc para la función main va allí */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
