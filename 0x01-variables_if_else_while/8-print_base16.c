ebd8814fae02b80d105cd9ccd0afb5be0fc5b630
Alian416-code <2051@holbertonschool.com>
2020-06-15 20:43:25 +0000

    Alphabet and numbers in a hexadecimal base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+
+	for (y = 97; y <= 102; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
