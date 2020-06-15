6799b9431d35d4816b6526a165995c32c019f605
Alian416-code <2051@holbertonschool.com>
2020-06-15 04:10:10 +0000

    Numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+			}
+	printf("\n");
+	return (0);
+}
