36ad4b9436bff7782758d592f83c4fbf8ddcb650
Alian416-code <2051@holbertonschool.com>
2020-06-18 03:14:02 +0000

    c is lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha - letter uppercase o lowercase
+ * @c: alpha character
+ * Return: 1 if c is an alpha character, 0 if is not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
