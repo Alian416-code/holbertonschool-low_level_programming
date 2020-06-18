81fe4d8fe3adf1e8b0eb9e7d2750b04e6634c845
Alian416-code <2051@holbertonschool.com>
2020-06-18 20:50:13 +0000

    mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - multiplies integers
+ * @a: int to be multiplies
+ * @b: int to be multiplies
+ * Return: 1 if c is a digit else 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
