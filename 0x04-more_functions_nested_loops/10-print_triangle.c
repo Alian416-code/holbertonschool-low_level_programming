6813d6740adeb698ab2c846c2d3d3eba7bdf84a2
Alian416-code <2051@holbertonschool.com>
2020-06-24 17:46:42 +0000

    printing a triangle with # and spaces

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_triangle - print a triangle with # and spaces
+ * @size: the size of the triangle
+ * Return: No return values
+ */
+void print_triangle(int size)
+{
+	int c, s, l;
+
+	if (size > 0)
+	{
+		for (l = 1; l < size; s++)
+		{
+			for (s = 0; s < (size - l); s++)
+			{
+				_putchar(' ');
+			}
+			for (c = 0; c < l; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
