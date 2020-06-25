af553704507acc9609c41a670baf0f5c928d9b0f
Alian416-code <2051@holbertonschool.com>
2020-06-25 03:26:43 +0000

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - reset to 98 the n number
+ * @n: The variable pointer
+ * Return: No return value
+ */
+void reset_to_98(int *n)
+{
+	int i;
+
+	if (i == 98)
+		n = &i;
+	else
+		*n = 98;
+}
